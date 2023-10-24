bf6b0f55eefa8f183aba68af859458989187a6e3
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-10-24 13:14:57 -0400

    PHASE TWO Program is classified in unit objects (Matrices, creating, initializing, getting Rows/Cols/Elts, Priting and Freeing Operations), and a collection objects which calls the unit object do a very basic operation.

---
+++ BasicOperations.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinearDefs.h"
+
+
+
+Matrix* addMatrices(int, Matrix**);
+Matrix* subtractMatrix(int, Matrix**);
+Matrix* scalarMultiply (Matrix*, double);
+Matrix* transposeMatrix(Matrix*);
+
+
