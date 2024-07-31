97aadbefbb9ee5eff6ecee0620ad6d19692e36a5
jonathan <jonathan03ant@gmail.com>
2024-07-31 16:25:46 -0400

    Name change

---
+++ Matrix/BasicOps.h
@@ -0,0 +1,16 @@
+#ifndef BASICOPS_H
+#define BASICOPS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Matrix_i.h"
+
+
+
+Matrix* addMatrices(int, Matrix**);
+Matrix* subtractMatrix(int, Matrix**);
+Matrix* scalarMultiply (Matrix*, double);
+Matrix* transposeMatrix(Matrix*);
+
+#endif
+
