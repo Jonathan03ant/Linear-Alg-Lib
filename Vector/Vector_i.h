c06255fb19bb39b7cc2c42be694f5088742120f6
jonathan03ant <jonathan03ant@gmail.com>
2024-08-26 22:18:41 -0400

    func header

--- Vector/Vector_i.h
+++ Vector/Vector_i.h
@@ -1,6 +1,19 @@
 #ifndef VECTOR_I_H
 #define VECTOR_I_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct {
+	double* vector;
+	int size
+}Vector;
 
+Vector* createVector(int);
+Vector* initVector(Vector*, double*);
+double getElement(Vector*, int);
+int getSize(Vector*);
+
+void printVector(Vector*);
+void freeVector(Vector*);
 #endif
