e8c2e6c926f7a260363a48ed873becf5131a10d5
jonathan03ant <jonathan03ant@gmail.com>
2024-08-27 13:30:40 -0400

    BasicOps header defined

---
+++ Vector/BasicOpsV.h
@@ -0,0 +1,12 @@
+#include "Vector_i.h"
+#include <math.h>
+
+
+Vector* addVectors(int, Vector**);
+Vector* subtractVectors(int, Vector**);
+Vector* scalarMultiplyVector(Vector*, double);
+
+Vector* findUnitVector(Vector*);
+double dotProduct(Vector*, Vector*);
+double findCosine(Vector*, Vector*);
+Vector* crossProduct(Vector*, Vector*);//not implemented yet
