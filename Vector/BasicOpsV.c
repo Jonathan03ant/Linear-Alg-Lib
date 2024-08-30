2b33c31fa3adccb366076c5c0844d26bd8261c82
jonathan03ant <jonathan03ant@gmail.com>
2024-08-29 21:41:48 -0400

    Subbing Vectors

--- Vector/BasicOpsV.c
+++ Vector/BasicOpsV.c
@@ -23,5 +23,36 @@ Vector* addVectors (int num, Vector** vectors){
     }
 
     return result;
+}
 
+Vector* subtracktVectors(int num, Vector** vectors){
+    if (num < 0 || vectors == NULL){
+        perror("Not enough vectors for this Operation");
+    }
+
+    int size = vectors[0]->size;
+    Vector* result = createVector(size);
+
+    if (result == NULL){
+        perror("Failed to allocate memory for the result vector");
+        return NULL;
+    }
+
+    /*
+        * Now we can loop through numVectors and add them all together
+        * before that, we need to copy the elements of the first Vector in our result Vector 
+    */
+
+
+    for (int i = 0; i < num; i++){
+        for (int j = 0; j < size; j++){
+            if (i == 0){
+                result->vector[j] = vectors[i]->vector[j];
+            }else{
+                result->vector[j] -= vectors[i]->vector[j];
+            }
+        }
+    }
+
+    return result;
 }
