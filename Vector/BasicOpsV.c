3e6ba8edadaf3834979bdd8d6e31fb5038ed26b1
jonathan03ant <jonathan03ant@gmail.com>
2024-08-29 21:33:53 -0400

    Adding Vectors

--- Vector/BasicOpsV.c
+++ Vector/BasicOpsV.c
@@ -1,6 +1,27 @@
 #include "BasicOpsV.h"
 
+Vector* addVectors (int num, Vector** vectors){
+    if (num < 0 || vectors == NULL){
+        perror("Not enough vectors for this Operation");
+    }
 
+    int size = vectors[0]->size;
+    Vector* result = createVector(size);
 
-Vector* addVectors(int nums, Vector** Vectors){
+    if (result == NULL){
+        perror("Failed to allocate memory for the result vector");
+        return NULL;
+    }
 
+    /*
+        * Now we can loop through numVectors and add them all together
+    */
+    for (int i = 0; i < num; i++){
+        for (int j = 0; j < size; j++){
+            result->vector[j] += vectors[i]->vector[j];
+        }
+    }
+
+    return result;
+
+}
