4d5c6cb5c63ae70d81fb9f0a11161b23f212e26f
jonathan03ant <jonathan03ant@gmail.com>
2024-08-30 17:16:43 -0400

    _SO source edited

--- Matrix/MatrixLib.py
+++ Matrix/MatrixLib.py
@@ -1,7 +1,7 @@
 import ctypes
 
 # Load the shared object file
-matrix = ctypes.CDLL('/home/jonathan/UTG_SRVR/Projects/Linear-Alg-Lib/Matrix/_Matrix.so')
+matrix = ctypes.CDLL('/app/Matrix/_Matrix.so')
 
 # Define the Matrix struct
 class Matrix(ctypes.Structure):
